9bf8e42df48596a14290eb05d07fea6786e56674
honanaipr <honanaipr@github.com>
2024-01-14 02:07:07 +0300

    move handlesr to separate router

--- lisum_chat/app.py
+++ lisum_chat/app.py
@@ -15,12 +15,8 @@ redmine = Redmine(config.redmine.url, key=config.redmine.key)
 
 from . import redmine
 
+from .routers.main_router import main_router
 
-@dp.message(F.text.as_("message_text"))
-async def message_handler(message: types.Message, message_text: str) -> None:
-    result_message = await message.answer("⏳✍")
-    search_result = redmine.search(message_text).results[0].model_dump_json()
-    await result_message.edit_text(search_result, parse_mode=None)
-
+dp.include_router(main_router)
 
 bot = Bot(config.bot.token, parse_mode=ParseMode.HTML)
---
+++ lisum_chat/routers/main_router.py
@@ -0,0 +1,11 @@
+from aiogram import Router, types, F
+from .. import redmine
+
+main_router = Router()
+
+
+@main_router.message(F.text.as_("message_text"))
+async def message_handler(message: types.Message, message_text: str) -> None:
+    result_message = await message.answer("⏳✍")
+    search_result = redmine.search(message_text).results[0].model_dump_json()
+    await result_message.edit_text(search_result, parse_mode=None)
