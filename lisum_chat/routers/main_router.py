dc1664edf43e5bd3858961c68d38c467633f1875
honanaipr <honanaipr@github.com>
2024-01-14 02:47:31 +0300

    error hendeling

--- lisum_chat/routers/main_router.py
+++ lisum_chat/routers/main_router.py
@@ -7,5 +7,14 @@ main_router = Router()
 @main_router.message(F.text.as_("message_text"))
 async def message_handler(message: types.Message, message_text: str) -> None:
     result_message = await message.answer("⏳✍")
-    search_result = (await redmine.search(message_text)).results[0].model_dump_json()
-    await result_message.edit_text(search_result, parse_mode=None)
+    try:
+        search_result = [
+            result.url for result in (await redmine.search(message_text)).results[:3]
+        ]
+        search_result = "\n".join(search_result)
+        assert search_result
+    except Exception as e:
+        await result_message.edit_text(f"❗️ Ошибка\n{e}", parse_mode=None)
+        print(e)
+    else:
+        await result_message.edit_text(search_result, parse_mode=None)
