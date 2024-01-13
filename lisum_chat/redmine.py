81cfecc4118b5869940100ec7355fcbb37d6a137
honanaipr <honanaipr@github.com>
2024-01-14 02:27:43 +0300

    make requests async

--- lisum_chat/redmine.py
+++ lisum_chat/redmine.py
@@ -2,7 +2,6 @@ from .config import config
 import httpx
 import json
 import enum
-import typing
 from .schemas.redmine_schemas import SearchResponse
 
 
@@ -10,16 +9,17 @@ class FunctionType(enum.Enum):
     search = enum.auto()
 
 
-def request_json(function: FunctionType, query: str) -> typing.Any:
-    r = httpx.get(
-        config.redmine.url + "search.json",
-        params={"q": query, "key": config.redmine.key},
-    )
-    return r.json()
+async def request_json(function: FunctionType, query: str):
+    async with httpx.AsyncClient() as client:
+        r = await client.get(
+            config.redmine.url + "search.json",
+            params={"q": query, "key": config.redmine.key},
+        )
+        return r.json()
 
 
-def search(query: str) -> SearchResponse:
-    result = request_json(FunctionType.search, query=query)
+async def search(query: str) -> SearchResponse:
+    result = await request_json(FunctionType.search, query=query)
     return SearchResponse.model_validate(result)
 
 
--- lisum_chat/routers/main_router.py
+++ lisum_chat/routers/main_router.py
@@ -7,5 +7,5 @@ main_router = Router()
 @main_router.message(F.text.as_("message_text"))
 async def message_handler(message: types.Message, message_text: str) -> None:
     result_message = await message.answer("⏳✍")
-    search_result = redmine.search(message_text).results[0].model_dump_json()
+    search_result = (await redmine.search(message_text)).results[0].model_dump_json()
     await result_message.edit_text(search_result, parse_mode=None)
