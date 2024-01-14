88c84d3e00ba058347753d011c8ebd63a7719efc
honanaipr <honanaipr@github.com>
2024-01-15 02:10:50 +0300

    update model

--- lisum_chat/models/estimates_model.py
+++ lisum_chat/models/estimates_model.py
@@ -1,9 +1,17 @@
 from ..database import Base
 from sqlalchemy.orm import Mapped, mapped_column
+from typing import Literal
+from sqlalchemy.dialects.sqlite.json import JSON
 
 
 class Estimate(Base):
     __tablename__ = "estimate"
     id: Mapped[int] = mapped_column(primary_key=True)
-    reply: Mapped[str]
-    estimate: Mapped[bool]
+    responce_message_id: Mapped[int]
+    responce_chat_id: Mapped[int]
+    query_text: Mapped[str]
+    responce_text: Mapped[str]
+    responce_urls: Mapped[list[str] | None] = mapped_column(JSON, default=[])
+    enhancement_text: Mapped[str] = mapped_column(default="")
+    enhancement_urls: Mapped[list[str]] = mapped_column(JSON, default=[])
+    estimate: Mapped[Literal["good", "bad"] | None] = mapped_column(default=None)
