1aef689f9b79daca6269033deb0c4e589255b33b
honanaipr <honanaipr@github.com>
2024-01-15 02:13:52 +0300

    interaction with new model

--- lisum_chat/crud/estimate_crud.py
+++ lisum_chat/crud/estimate_crud.py
@@ -1,7 +1,32 @@
 from ..models.estimates_model import Estimate
 from sqlalchemy.orm import Session
+from sqlalchemy import select
+from typing import Literal
 
 
-def add_estimate(session: Session, reply: str, estimate: bool):
-    db_estimate = Estimate(reply=reply, estimate=estimate)
+def add_response(
+    session: Session,
+    query_text: str,
+    responce_text: str,
+    message_id: int,
+    chat_id: int,
+):
+    db_estimate = Estimate(
+        query_text=query_text,
+        responce_text=responce_text,
+        responce_message_id=message_id,
+        responce_chat_id=chat_id,
+    )
     session.add(db_estimate)
+
+
+def add_estimate(
+    session: Session, chat_id: int, message_id: int, estimate: Literal["good", "bad"]
+):
+    stmt = (
+        select(Estimate)
+        .where(Estimate.responce_chat_id == chat_id)
+        .where(Estimate.responce_message_id == message_id)
+    )
+    db_estimate = session.scalars(stmt).one()
+    db_estimate.estimate = estimate
--- lisum_chat/routers/main_router.py
+++ lisum_chat/routers/main_router.py
@@ -2,14 +2,14 @@ from aiogram import Router, types, F
 from .. import redmine
 from ..markups.main_markup import estimates_markup
 from ..database import SessionLocal
-from ..crud.estimate_crud import add_estimate
+from ..crud.estimate_crud import add_estimate, add_response
 
 main_router = Router()
 
 
 @main_router.message(F.text.as_("message_text"))
 async def message_handler(message: types.Message, message_text: str) -> None:
-    result_message = await message.answer("⏳✍")
+    result_message = await message.reply("⏳✍")
     try:
         search_result = [
             result.url for result in (await redmine.search(message_text)).results[:3]
@@ -20,9 +20,23 @@ async def message_handler(message: types.Message, message_text: str) -> None:
         await result_message.edit_text(f"❗️ Ошибка\n{e}", parse_mode=None)
         print(e)
     else:
-        await result_message.edit_text(
-            search_result, parse_mode=None, reply_markup=estimates_markup
-        )
+        try:
+            with SessionLocal() as session:
+                add_response(
+                    session=session,
+                    query_text=message_text,
+                    responce_text=search_result,
+                    message_id=message.message_id,
+                    chat_id=message.chat.id,
+                )
+                session.commit()
+        except Exception as e:
+            await result_message.edit_text(f"❗️ Ошибка\n{e}", parse_mode=None)
+            print(e)
+        else:
+            await result_message.edit_text(
+                search_result, parse_mode=None, reply_markup=estimates_markup
+            )
 
 
 @main_router.callback_query()
@@ -34,11 +48,21 @@ async def my_callback_foo(query: types.CallbackQuery):
     assert query.message.text
     if query.data == "+":
         with SessionLocal() as session:
-            add_estimate(session=session, reply=query.message.text, estimate=True)
+            add_estimate(
+                session=session,
+                chat_id=query.message.chat.id,
+                message_id=query.message.reply_to_message.message_id,
+                estimate="good",
+            )
             session.commit()
         await query.answer(text="👍", show_alert=False)
     if query.data == "-":
         with SessionLocal() as session:
-            add_estimate(session=session, reply=query.message.text, estimate=False)
+            add_estimate(
+                session=session,
+                chat_id=query.message.chat.id,
+                message_id=query.message.message_id,
+                estimate="bad",
+            )
             session.commit()
         await query.answer(text="👎", show_alert=False)
