84329d8898b9bf66c1d4618381657394ef7536e9
honanaipr <honanaipr@github.com>
2024-01-15 03:12:14 +0300

    dry my_callback_foo

--- lisum_chat/routers/main_router.py
+++ lisum_chat/routers/main_router.py
@@ -56,23 +56,15 @@ async def my_callback_foo(
         await query.message.edit_reply_markup(reply_markup=None)
     else:
         return
+    with SessionLocal() as session:
+        add_estimate(
+            session=session,
+            chat_id=chat_id,
+            message_id=original_message_id,
+            estimate=query_data,
+        )
+        session.commit()
     if query_data == "good":
-        with SessionLocal() as session:
-            add_estimate(
-                session=session,
-                chat_id=chat_id,
-                message_id=original_message_id,
-                estimate="good",
-            )
-            session.commit()
         await query.answer(text="👍", show_alert=False)
-    if query_data == "bad":
-        with SessionLocal() as session:
-            add_estimate(
-                session=session,
-                chat_id=chat_id,
-                message_id=original_message_id,
-                estimate="bad",
-            )
-            session.commit()
+    else:
         await query.answer(text="👎", show_alert=False)
